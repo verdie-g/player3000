17ec752a82347745ae633681c2e52d0328990cee
verdie-g <gregoire.verdier@gmail.com>
2019-01-27 22:17:28 +0100

    server: add log level in config

--- server/src/util/Logger.ts
+++ server/src/util/Logger.ts
@@ -1,7 +1,9 @@
 import * as winston from 'winston';
+import * as config from 'config';
 
 export const logger: winston.Logger = winston.createLogger({
   exitOnError: false,
+  level: config.get('logLevel'),
   transports: [
     new winston.transports.Console(),
   ],
