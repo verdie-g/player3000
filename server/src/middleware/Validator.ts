2867602572d7bd1d268562ed6e7a0e0cc58ba3d4
verdie-g <gregoire.verdier@gmail.com>
2019-02-26 11:48:51 +0100

    server: optimize object validation

--- server/src/middleware/Validator.ts
+++ server/src/middleware/Validator.ts
@@ -1,29 +1,29 @@
 import * as Joi from 'joi';
 import * as Router from 'koa-tree-router';
 import { ParameterSchemas } from './Route';
-import { copyValues } from '../util/ObjectUtil';
 
-function validateObject(o: any, ctx: Router.IRouterContext, schema?: Joi.SchemaMap): boolean {
+function validateObject(obj: any, setObj: (ctx: Router.IRouterContext, obj: any) => void,
+  ctx: Router.IRouterContext, schema?: Joi.SchemaMap): boolean {
   if (schema === undefined) {
     return true;
   }
 
-  const result = Joi.validate(o, schema);
+  const result = Joi.validate(obj, schema);
   if (result.error !== null) {
     ctx.status = 400;
     ctx.body = result.error.details;
     return false;
   }
 
-  copyValues(o, result.value);
+  setObj(ctx, result.value);
   return true;
 }
 
 export function validate(paramSchemas: ParameterSchemas): Router.IMiddleware {
   return async (ctx: Router.IRouterContext, next: () => Promise<any>) => {
-    if (!validateObject(ctx.params, ctx, paramSchemas.params)
-      || !validateObject(ctx.request.body, ctx, paramSchemas.body)
-      || !validateObject(ctx.request.query, ctx, paramSchemas.query)) {
+    if (!validateObject(ctx.params, (ctx, obj) => ctx.params = obj, ctx, paramSchemas.params)
+      || !validateObject(ctx.request.body, (ctx, obj) => ctx.request.body = obj, ctx, paramSchemas.body)
+      || !validateObject(ctx.request.query, (ctx, obj) => ctx.request.query = obj, ctx, paramSchemas.query)) {
       return;
     }
 
